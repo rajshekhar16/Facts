8ab76804e0d136302f3cccd8877d571e846fad07
Raj Shekhar <rajshekhar16@gmail.com>
2018-02-28 03:14:10 +0530

    Initial Commit

---
+++ Facts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Facts
+//
+//  Created by Raj Shekhar on 2/28/18.
+//  Copyright © 2018 Raj Shekhar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
