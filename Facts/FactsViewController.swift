6df443b79e711d3e27a65600206d9d3702027a3e
Raj Shekhar <rajshekhar16@gmail.com>
2018-03-02 04:37:53 +0530

    Added reachability class and error handling

--- Facts/FactsViewController.swift
+++ Facts/FactsViewController.swift
@@ -30,6 +30,17 @@ class FactsViewController: UITableViewController {
  
     }
     
+    func checkForNetConnection() -> Bool
+    {
+        if !Reachability.isConnectedToNetwork(){
+            let alert = UIAlertController(title: "No Internet Connection", message: "Make sure your device is connected to the internet. Once connected pull to refresh", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: .cancel, handler: nil))
+            self.present(alert, animated: true)
+            return false
+        }
+        return true
+    }
+    
     // To show activity Indicator
     func showActivityIndicator()
     {
@@ -49,34 +60,43 @@ class FactsViewController: UITableViewController {
     /// Responsible to load data into table and also refresh when pull the tableView
     @objc func loadDataInToTable(refreshControl :UIRefreshControl? = nil)
     {
-
-        networkObj.clearImageCache()
-        networkObj.load(url) { (fact) in
-            
-            if let title = fact?.title
-            {
-                self.navigationController?.navigationBar.topItem?.title = title;
+        let isConnected = self.checkForNetConnection()
+        if isConnected
+        {
+            networkObj.clearImageCache()
+            networkObj.load(url) { (fact) in
                 
-            }
-            if let factData = fact?.rows
-            {
-                // remove entry from model if all objects are nil
-                self.factsDetails = factData.filter({ $0.imageURL != nil || ($0.desc != nil)  || $0.title != nil })
-            }
-            self.dataSource = FactsDataSource(factsData: self.factsDetails!)
-            DispatchQueue.main.async {
-                
-                self.hideActivityIndicator()
-                self.tableView.dataSource = self.dataSource
-                self.tableView.rowHeight = UITableViewAutomaticDimension
-                self.tableView.reloadData()
-                if refreshControl != nil{
-                    refreshControl?.endRefreshing()
+                if fact != nil
+                {
+                    if let title = fact?.title
+                    {
+                        self.navigationController?.navigationBar.topItem?.title = title;
+                        
+                    }
+                    if let factData = fact?.rows
+                    {
+                        // remove entry from model if all objects are nil
+                        self.factsDetails = factData.filter({ $0.imageURL != nil || ($0.desc != nil)  || $0.title != nil })
+                    }
+                    self.dataSource = FactsDataSource(factsData: self.factsDetails!)
+                    DispatchQueue.main.async {
+                        
+                        self.hideActivityIndicator()
+                        self.tableView.dataSource = self.dataSource
+                        self.tableView.rowHeight = UITableViewAutomaticDimension
+                        self.tableView.reloadData()
+                        if refreshControl != nil{
+                            refreshControl?.endRefreshing()
+                        }
+                        
+                        
+                    }
+                    
                 }
-                
-                
             }
-            
+        }
+        else{
+            refreshControl?.endRefreshing()
         }
         
     }
